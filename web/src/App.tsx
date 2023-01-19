a09529605a88449e331e164698bb45ca19898349
Paulo Capelo <paulodcsc@gmail.com>
2023-01-18 22:48:56 -0300

    status setup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,14 @@ import "./styles/global.css";
 
 function App() {
   return (
-    <div className="flex">
-      <Habit completed={1} />
-      <Habit completed={5} />
-      <Habit completed={3} />
+    <div>
+      <Habit completed="oi" />
+      <Habit completed="pedro" />
+      <Habit completed="feioso" />
+      <Habit completed="meu" />
+      <Habit completed="amorzinho" />
+      <Habit completed="feioso" />
+
     </div>
   );
 }
--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -1,10 +1,10 @@
 interface iHabit {
-  completed: number;
+  completed: string;
 }
 
 export function Habit(props: iHabit) {
   return (
-    <p className="bg-indigo-600 text-indigo-200 w-10 text-center m-1">
+    <p className="bg-rose-600 text-white w-28 text-center m-1">
       {props.completed}
     </p>
   );
